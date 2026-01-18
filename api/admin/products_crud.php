3edf5a41e45fff39896761b62d75b00800ac4482
OxidiLily <hadiprio575@gmail.com>
2026-01-18 16:53:30 +0700

    edit CRUD ADMIN

--- api/admin/products_crud.php
+++ api/admin/products_crud.php
@@ -1,10 +1,42 @@
 <?php
 include '../config.php';
 
-$action = $_GET['action'];
+$action = $_GET['action'] ?? '';
 
-if ($action == "read") {
-  $q = mysqli_query($conn,"SELECT * FROM products");
-  while ($r = mysqli_fetch_assoc($q)) $data[]=$r;
-  echo json_encode($data);
+switch($action){
+
+  case "read":
+    $q = mysqli_query($conn,"SELECT * FROM products");
+    $data = [];
+    while ($r = mysqli_fetch_assoc($q)) $data[]=$r;
+    echo json_encode(["status"=>"success","data"=>$data]);
+  break;
+
+  case "add":
+    $name  = $_POST['name'];
+    $price = $_POST['price'];
+    $stock = $_POST['stock'];
+
+    mysqli_query($conn,"INSERT INTO products (name,price,stock) VALUES ('$name','$price','$stock')");
+    echo json_encode(["status"=>"success","message"=>"Produk ditambahkan"]);
+  break;
+
+  case "update":
+    $id    = $_POST['id'];
+    $name  = $_POST['name'];
+    $price = $_POST['price'];
+    $stock = $_POST['stock'];
+
+    mysqli_query($conn,"UPDATE products SET name='$name', price='$price', stock='$stock' WHERE id='$id'");
+    echo json_encode(["status"=>"success","message"=>"Produk diupdate"]);
+  break;
+
+  case "delete":
+    $id = $_POST['id'];
+    mysqli_query($conn,"DELETE FROM products WHERE id='$id'");
+    echo json_encode(["status"=>"success","message"=>"Produk dihapus"]);
+  break;
+
+  default:
+    echo json_encode(["status"=>"error","message"=>"Action tidak valid"]);
 }
